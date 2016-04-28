0862c0744c8438e31390a48add57c858debf74a7
AviD <alex@gorbylev.ru>
2016-04-28 20:01:20 +0300

    fixes

--- src/Main/Base/StorableFile.php
+++ src/Main/Base/StorableFile.php
@@ -10,6 +10,7 @@ use Exception;
 use Hesper\Core\Base\IdentifiableObject;
 use Hesper\Core\Base\Timestamp;
 use Hesper\Core\Exception\WrongArgumentException;
+use Hesper\Core\Exception\WrongStateException;
 use Hesper\Main\DAO\Events\OnAfterDrop;
 use Hesper\Main\DAO\Events\OnBeforeSave;
 use Hesper\Main\Util\Storage\Engines\StorageEngine;
@@ -314,6 +315,16 @@ abstract class StorableFile extends IdentifiableObject implements OnBeforeSave, 
 		    $link = str_replace(['http:', 'https:'], '', $link);
 	    }
 	    return $link;
+    }
+
+    public function getPath() {
+        if( $this->getBaseStorageEngineTypeId()!=StorageEngineType::LOCAL ) {
+            throw new WrongStateException();
+        }
+        $config = StorageConfig::me()->getConfig(StorageEngineType::create($this->getBaseStorageEngineTypeId()), $this->getBaseStorageEngineConfig());
+        $path = realpath($config['path'].DIRECTORY_SEPARATOR.$this->getFileName());
+        return $path;
+
     }
 
     public function getFile() {
