49d52d30a903f5e8a0ac70361b130b7a1fcd1d65
AviD <alex@gorbylev.ru>
2016-08-26 18:55:06 +0300

    doc fix

--- src/Main/SPL/SimpleIndexedList.php
+++ src/Main/SPL/SimpleIndexedList.php
@@ -7,6 +7,13 @@ namespace Hesper\Main\SPL;
 
 class SimpleIndexedList extends AbstractList {
 
+	/**
+	 * @return SimpleIndexedList
+	 */
+	public static function create() {
+		return new self;
+	}
+
 	public function offsetSet($offset, $value) {
 		$this->list[$offset] = $value;
 
